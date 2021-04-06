3f722ed2bc6241a0dd0f48ed5d1dfca8c887ecc0
Mark Cooper <mark.c.cooper@outlook.com>
2021-04-06 08:52:51 -0700

    Make id match configurable

--- handler.rb
+++ handler.rb
@@ -3,7 +3,6 @@
 load 'vendor/bundle/bundler/setup.rb'
 require 'archivesspace/client'
 require 'logger'
-require 'uri'
 
 # - Setup connections (source and dest)
 # - Verify source and destination repositories exist
@@ -16,28 +15,49 @@ require 'uri'
 # TODO: notifications for failures
 $logger = Logger.new($stdout)
 
-def migrator(event:, context:)
-  since  = modified_since(event['recent_only']).to_s
-  source = setup_client('source', event)
-  destination = setup_client('destination', event)
+ID_GENERATORS = {
+  # map each id_X value to an array entry ['id_0', 'id_1', 'id_2', 'id_3']
+  'four_part' => lambda { |resource|
+    JSON.generate [
+      resource.fetch('id_0'),
+      resource.fetch('id_1', nil),
+      resource.fetch('id_2', nil),
+      resource.fetch('id_3', nil)
+    ]
+  },
+  # map each id_X value to a single array entry ['id_0.id_1.id_2.id_3'] (default)
+  'smushed' => lambda { |resource|
+    JSON.generate([(0..3).map { |i| resource.fetch("id_#{i}", nil) }.compact.join('.')])
+  }
+}.freeze
 
-  $logger.info "using modified since: #{since}"
+def migrator(event:, context:)
+  source       = setup_client('source', event)
+  destination  = setup_client('destination', event)
+  since        = modified_since(event['recent_only']).to_s
+  id_generator = event.fetch('id_generator', 'smushed')
+
   $logger.info "using source: #{source.config.base_uri}"
   $logger.info "using destination: #{destination.config.base_uri}"
+  $logger.info "using modified since: #{since}"
+  $logger.info "using id generator: #{id_generator}"
 
   source.resources(query: { modified_since: since }).each do |resource|
     next unless resource['publish']
 
-    four_part_id = resolve_id(resource)
-    $logger.info "[source] procesing resource #{four_part_id}: #{resource['uri']}"
-    record = retrieve_resource_description(source, uri_to_id(resource['uri']))
+    title      = resource['title']
+    uri        = resource['uri']
+    identifier = ID_GENERATORS.fetch(id_generator).call(resource)
+
+    $logger.info "[source] using resource #{identifier} (#{title}): #{uri}"
+    record = retrieve_resource_description(source, uri_to_id(uri))
     next unless record
 
-    json = convert_record(destination, record, four_part_id)
+    json = convert_record(destination, record, identifier)
     next unless json
 
-    remove_existing_record(destination, four_part_id)
-    import_record(destination, json, four_part_id)
+    remove_existing_record(destination, identifier)
+    import_record(destination, json, identifier)
   end
 
   true # we're happy =)
@@ -45,16 +65,16 @@ end
 
 private
 
-def convert_record(destination, record, four_part_id)
+def convert_record(destination, record, identifier)
   base_repo = destination.config.base_repo
   destination.config.base_repo = nil
-  $logger.info "[destination] converting resource #{four_part_id} to importable json"
+  $logger.info "[destination] converting resource #{identifier} to importable json"
   response = destination.post('plugins/jsonmodel_from_format/resource/ead', record)
   unless response.result.success?
     if response.parsed['error'] == 'Sinatra::NotFound'
       raise '[destination] jsonmodel plugin is not installed'
     else
-      raise "[destination] error converting resource #{four_part_id} to json"
+      raise "[destination] error converting resource #{identifier} to json"
     end
   end
 
@@ -71,8 +91,8 @@ def fatal_error(message)
   raise message
 end
 
-def import_record(destination, record, four_part_id)
-  $logger.info "[destination] importing resource #{four_part_id}: #{destination.config.base_uri}"
+def import_record(destination, record, identifier)
+  $logger.info "[destination] importing resource #{identifier}: #{destination.config.base_uri}"
   destination.post('batch_imports', record)
 rescue StandardError => e
   $logger.error e.message
@@ -83,26 +103,15 @@ def modified_since(recent_only)
   recent_only ? (DateTime.now - 1.0).to_time.utc.to_i : 0
 end
 
-def remove_existing_record(destination, four_part_id)
-  response = destination.get('find_by_id/resources', { query: { 'identifier[]': four_part_id } })
+def remove_existing_record(destination, identifier)
+  response = destination.get('find_by_id/resources', { query: { 'identifier[]': identifier } })
   response.parsed['resources'].each do |ref|
-    $logger.info "[destination] deleting resource #{four_part_id}: #{ref['ref']}"
+    $logger.info "[destination] deleting resource #{identifier}: #{ref['ref']}"
     destination.delete File.join('resources', uri_to_id(ref['ref']))
   end
 rescue StandardError => e
   $logger.error e.message
   nil
-end
-
-def resolve_id(resource)
-  # JSON.generate [
-  #   resource.fetch('id_0'),
-  #   resource.fetch('id_1', nil),
-  #   resource.fetch('id_2', nil),
-  #   resource.fetch('id_3', nil)
-  # ]
-  # Note: the default importer smushes everything into id_0
-  JSON.generate([(0..3).map { |i| resource.fetch("id_#{i}", nil) }.compact.join('.')])
 end
 
 def retrieve_resource_description(source, id)
